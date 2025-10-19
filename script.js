594da65031def3dd334c3d2b9504bc7320d26759
dandyycihuyy <dandyahmad139@gmail.com>
2025-10-20 06:28:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: "Ghost",
             artist: "Justin Bieber",
-            src: "ghost.mp3",
+            src: "Ghost.mp3",
             album: "justin bieber.jpg",
             lyrics: [
                 { time: 5, text: "jir" },
